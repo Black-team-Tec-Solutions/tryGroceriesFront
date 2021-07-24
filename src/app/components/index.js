d12f6f1e2ad03817b7b6e8d91031cdbdeba0ed9e
luisve9 <luissamdoval.9@gmail.com>
2021-07-24 09:55:38 -0500

    fix routes bug

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -4,7 +4,6 @@ export {default as TextInput} from './TextInput';
 export {default as NavBar} from './NavBar';
 export {default as SubMenu} from './SubMenu';
 export {default as Button} from './Button';
-export {default as Welcome} from './Welcome/Welcome';
 export {default as Workout} from '../views/workOut/Workout'
 export {default as Routine} from '../views/routine/Routine'
 export {default as TrenSuperior} from '../views/cards/TrenSuperior'
